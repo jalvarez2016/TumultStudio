106dd08cc1f90252bb1b5cde7914e6460d2dc81f
Jason <jasonalvarezcoraiza@gmail.com>
2020-05-03 20:32:28 -0400

    added social impact intentions to about page, needs some styling though

--- src/About/Mission.js
+++ src/About/Mission.js
@@ -25,7 +25,16 @@ class Mission extends React.Component {
                 <h1>Our mission</h1>
                 <p> Here at Tumult Studio, we make games that are enjoyable. We value story in our games
                     and hope to provide engaging experiences. We make games in the hope that they leave a lasting
-                    impression that players will look back on.
+                    impression that players will look back on. When we say we want you to smile, we mean those 
+                    smiles that actually mean something not those fake ones that society tells you that you 
+                    should wear. Have fun!
+                </p>
+                <h1>Social Impact</h1>
+                <p> We wish to create impact through subtly. Why? Cause it's fun to make people feel things,
+                    specifically making people feel bad about something tha's not real but illicts real human emotion,
+                    and the great thing about that is that these tend to stick with you a lot more than other emotions.
+                    Don't worry though, we try end things off on a happy not cause the world can be tough so we try to
+                    do our part.
                 </p>
             </div>
         )
